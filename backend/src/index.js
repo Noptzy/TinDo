f20ff04320d7bea15440278732d5088079aac4ef
Noptzy <nopsy.zet@gmail.com>
2025-04-22 19:29:06 +0700

    update config database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-
+const sequelize = require('../src/config/database.js');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+})
